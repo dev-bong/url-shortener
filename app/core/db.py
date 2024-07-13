e76d71a76c555fd2efda205b37e023e06150eaa5
dev-bong <qhddnjswo011@gmail.com>
2024-07-14 02:37:59 +0900

    ♻️ crud 코드 리팩토링
    - dynamodb의 키, 아이템 딕셔너리 생성 시 중복되는
    로직을 함수로 추출

--- app/core/db.py
+++ app/core/db.py
@@ -4,6 +4,7 @@ from boto3.dynamodb.conditions import Key
 from app.core.config import settings
 
 
+# dynamodb 리소스
 db_resource = boto3.resource(
     "dynamodb",
     aws_access_key_id=settings.AWS_ACCESS_KEY,
--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,21 @@
 from mypy_boto3_dynamodb.service_resource import Table
 
 
-def create_short_url(table: Table, short_key: str, origin_url: str) -> None:
-    item = {"key": short_key, "sort_key": "aa", "origin_url": origin_url}
-    table.put_item(Item=item)
+def key_dict(pk: str, sk: str) -> dict:
+    # dictionary 형태의 키 조합 만들기 (pk + sk)
+    key_name = ("key", "sort_key")
+    return dict(zip(key_name, (pk, sk)))
 
 
-def read_origin_url(table: Table, short_key: str) -> dict:
-    return table.get_item(Key={"key": short_key, "sort_key": "aa"}).get("Item", {})
+def create_short_url(table: Table, pk: str, origin_url: str) -> None:
+    # 단축 url 생성
+    url_info = key_dict(pk, "info#")
+    url_info["origin_url"] = origin_url
+
+    table.put_item(Item=url_info)
+
+
+def read_url_info(table: Table, pk: str) -> dict:
+    # 단축 url 정보 조회
+    info_key = key_dict(pk, "info#")
+    return table.get_item(Key=info_key).get("Item", {})
